f9fe45015a8a2669e2b53178a80facffe9d67da5
Ayobami <ayodejiayobami99@gmail.com>
2022-09-13 16:27:25 -0700

    3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10x of each
+ * alphabet followed by a new line.
+ * return: always void
+ */
+void print_alphabet_x10(void)
+	{
+		char x;
+		int y = 0;
+		while y <= 9
+		{
+			for (x = 'a'; x <= 'z'; x++)
+			{
+			_putchar(x);
+			}
+		_putchar('\n');
+		y++
+		}
+
