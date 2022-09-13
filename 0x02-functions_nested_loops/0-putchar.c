d34793fc7bc5c1890ae1ff5a5c610a5d5b67c68d
Ayobami <ayodejiayobami99@gmail.com>
2022-09-13 12:01:25 -0700

    Commit 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,6 +4,14 @@ int main(void)
  * Return -Always 0
  */
 {
-	_putchar("_putchar\n")
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n")
 		return 0
 }
