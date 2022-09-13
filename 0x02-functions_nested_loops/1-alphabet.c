1cea6947fea943595c0fa39316d773c9e6c92c60
Ayobami <ayodejiayobami99@gmail.com>
2022-09-13 15:30:00 -0700

    2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - printsthe alohabets jn lowercase
+ * followed by a new line
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int char;
+		for (char='a'; char<='z'; char++)
+		{
+			_putchar(char);
+		}
+		return (0);
+}
