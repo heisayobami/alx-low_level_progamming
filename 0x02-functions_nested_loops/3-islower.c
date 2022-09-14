ff35e83cb7112debc19f42e7e4ba9ceeb0782698
Ayobami <ayodejiayobami99@gmail.com>
2022-09-13 17:17:47 -0700

    task

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,16 +2,16 @@
 
 /**
   * _islower - checks for lowercase character
-  * @c: character too be checked
+  * @c: character to be checked
   * Return: 1 for lower case character or 0 for anything else
   */
 
 int _islower(int c)
 {
-	if (c>= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
-	
+
 	return (0);
 }
