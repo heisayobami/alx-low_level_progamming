b1b3f8c0b2bdac52a237e1c280f44f8e7d34090a
Ayobami <ayodejiayobami99@gmail.com>
2022-10-06 14:30:51 -0700

    task

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+  * _calloc - allocates memory for an initialized buffer
+  * @nmemb: number of memory bytes
+  * @size: number of data type bits
+  * Return: return pointer to initialized buffer
+  */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *block;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	block = malloc(nmemb * size);
+	if (block != NULL)
+	{
+		for (i = 0; i < (nmemb * size); i++)
+			block[i] = 0;
+		return (block);
+	}
+	else
+		return (NULL);
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* array_range - creates an array of integers
+* @min: minimum value
+* @max: maximum value
+* Return: pointer to array of integers
+*/
+
+int *array_range(int min, int max)
+{
+	int *block;
+	int i, j = 0;
+
+	if (min > max)
+		return (NULL);
+	block = malloc(sizeof(*block) * ((max - min) + 1));
+	if (block != NULL)
+	{
+		for (i = min; i <= max; i++)
+		{
+			block[j] = i;
+			j++;
+		}
+		return (block);
+	}
+	else
+		return (NULL);
+
+}
