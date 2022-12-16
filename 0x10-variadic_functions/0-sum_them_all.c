c3a3b37f649f236adc5560b39e7d29f2240861c0
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2022-12-16 14:03:50 +0100

    .

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,11 +12,11 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
 	int i, sum = 0;
-
-	va_start(ap, n);
 	
 	if (n == 0)
 		{ return 0; }
+
+	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
