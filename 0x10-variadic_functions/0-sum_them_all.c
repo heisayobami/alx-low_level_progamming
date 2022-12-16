52bcbe2c176bc6b4e76044c20f1c72b9b4161405
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2022-12-16 13:56:18 +0100

    updated sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,19 +8,20 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list valist;
+	va_list parameter;
 	unsigned int i, sum = 0;
 
+	va_start(parameter, n);
+	
 	if (n == 0)
-		return (0);
-	va_start(valist, n);
+		{ return 0; }
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(valist, int);
+		sum += va_arg(parameter, int);
 	}
 
-	va_end(valist);
+	va_end(parameter);
 
-	return (sum);
+	return sum;
 }
